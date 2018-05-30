75435025cf837e4945e3c66ef29a2da3f6cc88af
jrambold <jrambold@gmail.com>
2018-05-30 12:00:02 -0600

    finalized

---
+++ spec/services/oxford_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe OxfordService do
+  context "Instance methods" do
+    context "get_url" do
+      it "returns json for valid word" do
+        search_service = OxfordService.new
+        raw_dictionary = search_service.get_url("/api/v1/inflections/en/foxes")
+        root = raw_dictionary[:results].first[:lexicalEntries].first[:inflectionOf].first[:id]
+        expect(root).to eq('fox')
+      end
+
+      it "returns nil for anything other than json" do
+        search_service = OxfordService.new
+        result = search_service.get_url("/api/v1/inflections/en/foxez")
+
+        expect(result).to be_nil
+      end
+    end
+  end
+end
